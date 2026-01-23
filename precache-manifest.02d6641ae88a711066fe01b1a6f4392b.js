c839c49c3a26998dd4dc54f2811a5fb79ed0b0b5
Luvvydev <Reneker54@yahoo.com>
2026-01-22 20:22:01 -0500

    Updates

---
+++ precache-manifest.02d6641ae88a711066fe01b1a6f4392b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215138e7eb998a83062d61e0f11baabf",
+    "url": "/opening-notation-drills/index.html"
+  },
+  {
+    "revision": "95c7286b136ab888d77a",
+    "url": "/opening-notation-drills/static/css/main.0645d30d.chunk.css"
+  },
+  {
+    "revision": "bbf55d934747b7b730b0",
+    "url": "/opening-notation-drills/static/js/2.220b5a67.chunk.js"
+  },
+  {
+    "revision": "4d74ce1e7fee0a16b28267ca01d74155",
+    "url": "/opening-notation-drills/static/js/2.220b5a67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c7286b136ab888d77a",
+    "url": "/opening-notation-drills/static/js/main.b25105f7.chunk.js"
+  },
+  {
+    "revision": "1a3a16b5468cd40166ee",
+    "url": "/opening-notation-drills/static/js/runtime-main.b145df27.js"
+  }
+]);
