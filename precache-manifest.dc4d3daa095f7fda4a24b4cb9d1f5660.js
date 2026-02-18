5a215ed902247cae11226980e3e86cedc650b36a
Luvvydev <Reneker54@yahoo.com>
2026-02-17 19:55:16 -0500

    Updates

---
+++ precache-manifest.dc4d3daa095f7fda4a24b4cb9d1f5660.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208856315e7b786b2579e859a20fbce4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f3b557a59ad41dab511",
+    "url": "/static/css/main.3cb10583.chunk.css"
+  },
+  {
+    "revision": "f83c9552f8b5dc8b59c6",
+    "url": "/static/js/2.539e49dc.chunk.js"
+  },
+  {
+    "revision": "4013abe0f5dd81b55f40ff99c931cbec",
+    "url": "/static/js/2.539e49dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f3b557a59ad41dab511",
+    "url": "/static/js/main.306aeac1.chunk.js"
+  },
+  {
+    "revision": "08b7cc41961a69c4fa96",
+    "url": "/static/js/runtime-main.c28dc3b4.js"
+  },
+  {
+    "revision": "48a60fec0c94edaab3a9e5a95246d0bd",
+    "url": "/static/media/chessdrillslogo.48a60fec.png"
+  }
+]);
