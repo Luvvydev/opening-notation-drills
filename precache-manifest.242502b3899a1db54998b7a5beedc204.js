b5ded0161753ea1e34fe88455c084eed0328ee6f
Luvvydev <Reneker54@yahoo.com>
2026-02-18 18:50:43 -0500

    Updates

--- precache-manifest.242502b3899a1db54998b7a5beedc204.js
+++ precache-manifest.242502b3899a1db54998b7a5beedc204.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3cf4e8a32e749d159cc57b54b74a854d",
+    "revision": "567f9405b6c14b55f95c28c7e88c40fa",
     "url": "./index.html"
   },
   {
-    "revision": "c4f3b9b44a521a1d9e93",
-    "url": "./static/css/main.3cb10583.chunk.css"
+    "revision": "a5d4cf6eab4c6abbfd4d",
+    "url": "./static/css/main.65ac79b5.chunk.css"
   },
   {
     "revision": "31adfd6185630c211a0f",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.950a3188.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c4f3b9b44a521a1d9e93",
-    "url": "./static/js/main.0f2ebd86.chunk.js"
+    "revision": "a5d4cf6eab4c6abbfd4d",
+    "url": "./static/js/main.9e9f2975.chunk.js"
   },
   {
     "revision": "5482cd39b51d096ff603",
