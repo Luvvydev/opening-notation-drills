363b5f58dbf261336473cbe3d81c016ab1c1ee4c
Luvvydev <Reneker54@yahoo.com>
2026-03-25 05:34:05 -0400

    Updates

---
+++ precache-manifest.204b2f47f681d1f636517cc1659f749e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aefa2559848e514996baf2795f55d25",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5bc464bd7a9f5a3ff9c4",
+    "url": "./static/css/main.1615e6d1.chunk.css"
+  },
+  {
+    "revision": "7177f6637ba68b4bf9cb",
+    "url": "./static/js/2.949be2bd.chunk.js"
+  },
+  {
+    "revision": "4013abe0f5dd81b55f40ff99c931cbec",
+    "url": "./static/js/2.949be2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bc464bd7a9f5a3ff9c4",
+    "url": "./static/js/main.1c696761.chunk.js"
+  },
+  {
+    "revision": "5482cd39b51d096ff603",
+    "url": "./static/js/runtime-main.d2f8e4d9.js"
+  },
+  {
+    "revision": "48a60fec0c94edaab3a9e5a95246d0bd",
+    "url": "./static/media/chessdrillslogo.48a60fec.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7c7f87a457aa15688cb9a1078e1d0061.js"
+  "./precache-manifest.204b2f47f681d1f636517cc1659f749e.js"
 );
 
 self.addEventListener('message', (event) => {
